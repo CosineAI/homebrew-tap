cd09abe4b258ed42bff72678608c7caac4e38b8d
Pandelis Zembashis <p@ndel.is>
2025-07-30 13:47:50 +0100

    chore: depends on

--- Formula/cos-nightly.rb
+++ Formula/cos-nightly.rb
@@ -7,7 +7,7 @@ class CosNightly < Formula
   version "nightly-117"
   license "Apache 2.0"
 
-  depends_on formula: ["fzf", "ripgrep"]
+  depends_on: ["fzf", "ripgrep"]
 
   on_macos do
     if Hardware::CPU.arm?

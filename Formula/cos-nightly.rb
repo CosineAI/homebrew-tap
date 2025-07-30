fa22dc2bf4c1b158396671ff8e8415b2fc80d9fa
Pandelis Zembashis <p@ndel.is>
2025-07-30 13:46:10 +0100

    chore: change the layout

--- Formula/cos-nightly.rb
+++ Formula/cos-nightly.rb
@@ -2,28 +2,43 @@
 # frozen_string_literal: true
 
 class CosNightly < Formula
+  desc "Nightly builds of the Cosine CLI"
+  homepage "https://cosine.sh/cli"
   version "nightly-117"
   license "Apache 2.0"
 
-  on_intel do
-    on_linux do
-      url "https://github.com/CosineAI/cli/releases/download/nightly/cosine-linux-amd64.zip"
-    end
-  end
-  on_arm do
-    on_macos do
-      url "https://github.com/CosineAI/cli/releases/download/nightly/cosine-darwin-arm64.zip"
-    end
-    on_linux do
-      url "https://github.com/CosineAI/cli/releases/download/nightly/cosine-linux-arm64.zip"
-    end
-  end
-  sha256 :no_check
-
-  name "Cosine CLI Nightly"
-  desc "Nightly builds of the Cosine CLI"
-
   depends_on formula: ["fzf", "ripgrep"]
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/CosineAI/cli/releases/download/nightly/cosine-darwin-arm64.zip"
+      sha256 :no_check
+      def install
+        bin.install "cosine" => "cos-nightly"
+      end
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/CosineAI/cli/releases/download/nightly/cosine-linux-amd64.zip"
+      sha256 :no_check
+      def install
+        bin.install "cosine" => "cos-nightly"
+      end
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/CosineAI/cli/releases/download/nightly/cosine-linux-arm64.zip"
+      sha256 :no_check
+      def install
+        bin.install "cosine" => "cos-nightly"
+      end
+     end
+   end
+  end
+
+
+
 
   bin.install "cos" => "cos-nightly"
 

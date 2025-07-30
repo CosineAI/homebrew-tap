e73e5cc838a6ed7805d40f3c49f4454ee1d45518
Pandelis Zembashis <p@ndel.is>
2025-07-30 11:52:10 +0100

    chore: fix url

--- Formula/cos.rb
+++ Formula/cos.rb
@@ -8,7 +8,7 @@ class Cos < Formula
 
   on_intel do
     on_linux do
-              url "https://github.com/CosineAI/cli/releases/download/0.1.2/cosine-darwin-amd64.zip"
+              url "https://github.com/CosineAI/cli/releases/download/0.1.2/cosine-linux-amd64.zip"
               sha256 "4cc6351f20199a347a90da7dff62592a998590b0bf2efd211e2f50a9b6892258"
     end
   end
@@ -18,7 +18,7 @@ class Cos < Formula
               sha256 "440532a7a230e7c6491bb3c1255000a45b5d71a3100eae8001c5057a4e861052"
     end
     on_linux do
-              url "https://github.com/CosineAI/cli/releases/download/0.1.2/cosine-darwin-arm64.zip"
+              url "https://github.com/CosineAI/cli/releases/download/0.1.2/cosine-linux-arm64.zip"
               sha256 "440532a7a230e7c6491bb3c1255000a45b5d71a3100eae8001c5057a4e861052"
     end
   end

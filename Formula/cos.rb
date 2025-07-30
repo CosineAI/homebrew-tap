9d0864e7d273bd1a96fb0a48283eb1f80e46a863
Pandelis Zembashis <p@ndel.is>
2025-07-30 13:52:16 +0100

    chore: new format

--- Formula/cos.rb
+++ Formula/cos.rb
@@ -1,31 +1,40 @@
+
 # typed: false
 # frozen_string_literal: true
 
 class Cos < Formula
+  desc "The Cosine CLI"
+  homepage "https://cosine.sh/cli"
   version "0.1.2"
   license "Apache 2.0"
 
-  on_intel do
-    on_linux do
-              url "https://github.com/CosineAI/cli/releases/download/0.1.2/cosine-linux-amd64.zip"
-              sha256 "4cc6351f20199a347a90da7dff62592a998590b0bf2efd211e2f50a9b6892258"
-    end
-  end
-  on_arm do
-    on_macos do
-              url "https://github.com/CosineAI/cli/releases/download/0.1.2/cosine-darwin-arm64.zip"
-              sha256 "440532a7a230e7c6491bb3c1255000a45b5d71a3100eae8001c5057a4e861052"
-    end
-    on_linux do
-              url "https://github.com/CosineAI/cli/releases/download/0.1.2/cosine-linux-arm64.zip"
-              sha256 "440532a7a230e7c6491bb3c1255000a45b5d71a3100eae8001c5057a4e861052"
+  depends_on ["fzf", "ripgrep"]
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/CosineAI/cli/releases/download/0.1.2/cosine-darwin-arm64.zip"
+      sha256 "440532a7a230e7c6491bb3c1255000a45b5d71a3100eae8001c5057a4e861052"
+      def install
+        bin.install "cosine" => "cos"
+      end
     end
   end
 
-  name "Cosine CLI"
-  desc "CLI for granting Genie access to your local machine"
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/CosineAI/cli/releases/download/0.1.2/cosine-linux-amd64.zip"
+      sha256 "4cc6351f20199a347a90da7dff62592a998590b0bf2efd211e2f50a9b6892258"
+      def install
+        bin.install "cosine" => "cos"
+      end
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/CosineAI/cli/releases/download/0.1.2/cosine-linux-arm64.zip"
+      sha256 "440532a7a230e7c6491bb3c1255000a45b5d71a3100eae8001c5057a4e861052"
+      def install
+        bin.install "cosine" => "cos"
+      end
+     end
+   end
 
-  depends_on formula: ["fzf", "ripgrep"]
-
-  bin.install "cos"
 end

48f67288e986b4f52e9bf2acbe011139d69a1821
Pandelis Zembashis <p@ndel.is>
2025-07-30 12:07:38 +0100

    chore: fix name

--- Formula/cos-nightly.rb
+++ Formula/cos-nightly.rb
@@ -1,7 +1,7 @@
 # typed: false
 # frozen_string_literal: true
 
-class Cos-nightly < Formula
+class CosNightly < Formula
   arch arm: "arm64", intel: "x86_64"
   version "nightly-117"
   licence "Apache 2.0"

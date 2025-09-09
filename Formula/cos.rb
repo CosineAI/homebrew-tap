d41da7981b4113a38db00862845dca7728975a01
Genie <genie@cosine.sh>
2025-09-09 14:05:54 +0000

    Update cosine to version 0.2.2

--- Formula/cos.rb
+++ Formula/cos.rb
@@ -4,15 +4,15 @@
 class Cos < Formula
   desc "The Cosine CLI"
   homepage "https://cosine.sh/cli"
-  version "0.2.1"
+  version "0.2.2"
   license "Apache 2.0"
 
   depends_on ["fzf", "ripgrep"]
 
   on_macos do
     if Hardware::CPU.arm?
-                              url "https://github.com/CosineAI/cli/releases/download/0.2.1/cosine-darwin-arm64.zip"
-                              sha256 "af3aba887ae305c2a3234b0e60d8b3366a1ff4261cc8a7473db3859f3b564bfd"
+                                    url "https://github.com/CosineAI/cli/releases/download/0.2.2/cosine-darwin-arm64.zip"
+                                    sha256 "e75f0e6cdc56ad7d94b590256da98445ec77afbaff17579d187db61fde88e6c9"
       def install
         bin.install "cosine" => "cos"
       end
@@ -21,15 +21,15 @@ class Cos < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-                              url "https://github.com/CosineAI/cli/releases/download/0.2.1/cosine-linux-amd64.zip"
-                              sha256 "f70ba0bb6b0e8c239311a8a25ee5365f68773a145ea60d8e9d7ac72f6104f58a"
+                                    url "https://github.com/CosineAI/cli/releases/download/0.2.2/cosine-linux-amd64.zip"
+                                    sha256 "3246f4d3d96ef6a4ed87a52267267ed0c9f2b45e352566a303845a6f0817242e"
       def install
         bin.install "cosine" => "cos"
       end
     end
     if Hardware::CPU.arm?
-                              url "https://github.com/CosineAI/cli/releases/download/0.2.1/cosine-linux-arm64.zip"
-                              sha256 "c42b1b2a26df2fa9fb2b67454d6fe8eb0b73b93c629ecb04ca0d96bb2e6b7c95"
+                                    url "https://github.com/CosineAI/cli/releases/download/0.2.2/cosine-linux-arm64.zip"
+                                    sha256 "00c298d76b984d7a9b7288572375c82e7935d1a0a318e51d86304e2bf4a40578"
       def install
         bin.install "cosine" => "cos"
       end

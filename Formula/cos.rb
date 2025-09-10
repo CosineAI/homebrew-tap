bea594d104fe695a0a3a8b8d7433e84c92c651b3
Pandelis Zembashis <p@ndel.is>
2025-09-10 13:47:10 +0100

    chore: fix cos binary

--- Formula/cos.rb
+++ Formula/cos.rb
@@ -14,7 +14,7 @@ class Cos < Formula
                                           url "https://github.com/CosineAI/cli/releases/download/1.0.0/cosine-darwin-arm64.zip"
                                           sha256 "7448c847db667060eb2d6f69c7809161d57f8f20c2f74245ceec5f67552cc12f"
       def install
-        bin.install "cosine" => "cos"
+        bin.install "cos"
       end
     end
   end
@@ -24,14 +24,14 @@ class Cos < Formula
                                           url "https://github.com/CosineAI/cli/releases/download/1.0.0/cosine-linux-amd64.zip"
                                           sha256 "afb15af6db3f2b997d3f939ed3aaf58301f7c89121a64aadec11919f362dbc95"
       def install
-        bin.install "cosine" => "cos"
+        bin.install "cos"
       end
     end
     if Hardware::CPU.arm?
                                           url "https://github.com/CosineAI/cli/releases/download/1.0.0/cosine-linux-arm64.zip"
                                           sha256 "a921b5184aca73f85dd4bef930afcebacaa44c18b4c1e86d1f214644aadcccc3"
       def install
-        bin.install "cosine" => "cos"
+        bin.install "cos"
       end
      end
    end

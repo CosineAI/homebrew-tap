965914f7e6ca1f5bdffbcfa1d1fb5a7bda0f5bac
Pandelis Zembashis <p@ndel.is>
2026-02-16 10:55:24 +0000

    chore: beta

---
+++ Formula/cos-beta.rb
@@ -0,0 +1,27 @@
+class CosBeta < Formula
+  desc "Beta builds of the Cosine CLI"
+  homepage "https://cosine.sh/cli"
+  version "nightly-145"
+  license "Apache 2.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      def install
+        bin.install "cos2" => "cos-beta"
+      end
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      def install
+        bin.install "cos2" => "cos-beta"
+      end
+    end
+    if Hardware::CPU.arm?
+      def install
+        bin.install "cos2" => "cos-beta"
+      end
+    end
+  end
+end

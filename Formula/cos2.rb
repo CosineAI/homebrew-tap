6494b0388d89fa9867a6265d4e4bf03fa67ddbe0
Pandelis Zembashis <p@ndel.is>
2026-03-04 14:17:12 +0000

    chore: fix

--- Formula/cos2.rb
+++ Formula/cos2.rb
@@ -5,13 +5,12 @@ class Cos2 < Formula
   desc "Nightly builds of the Cosine CLI"
   homepage "https://cosine.sh/cli"
   version "nightly-353"
-  license "Apache 2.0"
 
   on_macos do
     if Hardware::CPU.arm?
             url "https://github.com/CosineAI/cli2/releases/download/nightly/cos2-darwin-arm64.zip"
       def install
-        bin.install "cos2" => "cos2"
+        bin.install "cos" => "cos2"
       end
     end
   end
